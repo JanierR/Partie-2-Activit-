31d9eeca11bae24c29b0ca17f92b60d971869db1
JanierR <janier.2320@gmail.com>
2016-10-19 13:52:32 -0400

    Ajoute de fonction test_input dans form.php

--- form.php
+++ form.php
@@ -3,12 +3,20 @@
 $nom = mysqli_real_escape_string($con, $_POST['nom']); 
 $prenom = mysqli_real_escape_string($con, $_POST['prenom']);
 $courriel = mysqli_real_escape_string($con, $_POST['courriel']);
-								 		 //insert les valeurs dans la bd
+
+//insert les valeurs dans la bd
 $sql = "INSERT INTO contacts (firstname, lastname, email) VALUES ('$nom','$prenom','$courriel')";
 
 if(!mysqli_query($con, $sql)){
 	die('Erreur: ' . mysqli_error($con));
-										 } else {
+ } else {
 	header("Location: index.php");
+}
+
+function test_input($data) {
+  $data = trim($data);
+  $data = stripslashes($data);
+  $data = htmlspecialchars($data);
+  return $data;
 }
 ?>
